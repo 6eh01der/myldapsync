503a59b9fef48b6d2ac7304b8d7122b93e89eaee
Artur Lebedev <ras_atari@mail.ru>
2024-09-04 18:46:26 +0300

    More correct condition for GRANT PROXY for groups

--- myldapsync/__init__.py
+++ myldapsync/__init__.py
@@ -234,11 +234,8 @@ def main():
                     cur.execute('%s' % privilege_list)
                     cur.execute('%s' % user_grants)
                     cur.execute('%s' % user_admin_grants)
-                    if (filter_string := config.get('mysql', 'filter_string')) != '':
-                        if "(objectClass=group)" in filter_string or "(objectClass=groupOfNames)" in filter_string:
-                            cur.execute('GRANT PROXY ON "%s" TO ''@'';' % user_name)
-                    if (admin_filter_string := config.get('mysql', 'admin_filter_string')) != '':
-                        if "(objectClass=group)" in admin_filter_string or "(objectClass=groupOfNames)" in admin_filter_string:
+                    if (filter_string := config.get('mysql', 'filter_string')) != '' and \
+                        ("(objectClass=group)" in filter_string or "(objectClass=groupOfNames)" in filter_string):
                             cur.execute('GRANT PROXY ON "%s" TO ''@'';' % user_name)
                     users_added = users_added + 1
                 except mysql.connector.Error as exception:
